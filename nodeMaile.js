c88a79f23edfeed7bcdcccb62d784a5b83115d1c
Mateus Varela <mateus.varela@fw7.com.br>
2021-08-31 16:53:07 -0300

    feat: added date to know when the email is sended

--- nodeMaile.js
+++ nodeMaile.js
@@ -4,16 +4,26 @@ const sendEmail = async () => {
   require('dotenv').config();
   const senMail = require('@sendgrid/mail')
   senMail.setApiKey(process.env.SENDGRID_API_KEY)
+  const date = new Date().toLocaleDateString('pt-BR', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric',
+  })
+  console.log(date)
   const message = {
     to: 'mvarela038@gmail.com',
     from: 'mvarela038@gmail.com',
     subject: 'Enviando e-mail com SendGrid',
     text: 'and easy to do anywhere, even with Node.js',
-    html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+    html: `<strong>and easy to do anywhere, even with Node.js ${date}</strong>`,
   }
 
-  await senMail.send(message)
-  module.exports = cron.schedule("*/1 * * * *", sendEmail, {
-    schedule: false
-  })
+  console.log("Enviando email...")
+  return await senMail.send(message)
 }
+module.exports = cron.schedule("*/1 * * * *", sendEmail, {
+  schedule: false
+})
